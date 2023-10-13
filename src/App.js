c6e1a6ff0db3d9ff7aeca2f2b6536e2c0415b731
anisbc <bachaanis0000@gmail.com>
2023-10-13 21:56:04 +0100

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate, HashRouter} from 'react-router-dom';
 import { useState } from "react";
 import Layout from "./Layout";
 import Success from './Success';
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>  
-    <Router >
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </Router>
+     <HashRouter>
+      <App />
+    </HashRouter>
+
   </React.StrictMode>
 );
 
